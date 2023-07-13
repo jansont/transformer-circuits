f0b596c0397b3bcd23a97ee409347743f2219f2d
Theo Janson <tj@sfjtechnologies.com>
2023-07-13 15:17:47 +0200

    toml

--- colab_setup.sh
+++ colab_setup.sh
@@ -1,6 +1,9 @@
+
+
+
+
 pip install poetry
-poetry install
-poetry shell
+poetry config virtualenvs.in-project true
 
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -; sudo apt-get install -y nodejs
 pip install git+https://github.com/neelnanda-io/PySvelte.git
