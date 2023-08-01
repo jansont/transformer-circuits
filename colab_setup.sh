216cfcaf341f0f895ea1c9acbe1c658dedd90c28
Theo Janson <tj@sfjtechnologies.com>
2023-08-01 11:25:54 +0200

    transformer lens

--- colab_setup.sh
+++
@@ -1,9 +0,0 @@
-
-
-
-
-pip install poetry
-poetry install 
-
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -; sudo apt-get install -y nodejs
-pip install git+https://github.com/neelnanda-io/PySvelte.git
--- setup.sh
+++
@@ -1,7 +0,0 @@
-pip install -r requirements.txt
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -; sudo apt-get install -y nodejs
-pip install git+https://github.com/neelnanda-io/PySvelte.git
-
-%cd ../
-!bash setup.sh
-%cd notebooks
